31f08281cebf43d9f350dd90fe5a9afee71e1849
Jesus Lapastora <cyber.gsuscode@gmail.com>
2020-04-24 00:39:58 +0200

    Added comments

--- practices/sorting_algorithms/merge_sort.c
+++ practices/sorting_algorithms/merge_sort.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 
 void mergeSort(int values[], int start, int end) {
+    /*
+     * Sorts an array of values given a start and an
+     * end, using the recursive merge-sort algorithm.
+     * 
+     */
+
     int i;
     if (end - start < 2) return;
     int mid = (end + start) / 2;
@@ -10,6 +16,7 @@ void mergeSort(int values[], int start, int end) {
 
     int len_left = mid - start;
     int len_right = end - mid;
+
     int *left = (int *) malloc (sizeof (int) * len_left);
     int *right = (int *) malloc (sizeof (int) * len_right);
 
@@ -25,6 +32,21 @@ void mergeSort(int values[], int start, int end) {
         ri = 0,
         gi = start;
 
+
+    /*
+     * Merge : 
+     *  While none of the two arrays consumed:
+     *      1. Compare the two values from the arrays.
+     *      2. Get the smallest one into the original array.
+     *      3. Advance the selected index.
+     *      4. Advance the original array index.
+     *
+     * Once any of the two arrays is consumed,
+     * given that both arrays are sorted, just append
+     * the rest of the items to the original array as
+     * they are bigger than the rest.
+     */
+
     while (li < len_left && ri < len_right) {
         if (left[li] < right[ri])
             values[gi++] = left[li++];
@@ -38,20 +60,36 @@ void mergeSort(int values[], int start, int end) {
     while (ri < len_right)
         values[gi++] = right[ri++];
 
+    // Make sure to free the allocated
+    // pointers
     free(left);
     free(right);
 }
 
 int randrange(int min, int max) {
+    /* 
+     * Gets a random number and transfers it
+     * to the range expected.
+     *
+     * https://stackoverflow.com/questions/7560114/random-number-c-in-some-range
+     */
   return min + (rand() % ( max - min + 1));
 }
 
 void getRange(int *values, int start, int end) {
+    /*
+     * Inserts a range of values into an array,
+     * including `start` and excluding `end`.
+     */
     for (int i = start; i < end; ++i)
         values[i - start] = i;
 }
 
 void shuffle(int values[], int length) {
+    /*
+     * Shuffles an array of any given length,
+     * swapping values at random indexes.
+     */
     for (int i = length - 1; i >= 0; --i) {
         int index = randrange(0, length - 1);
 
@@ -62,6 +100,10 @@ void shuffle(int values[], int length) {
 }
 
 void printList(int values[], int start, int end) {
+    /*
+     * Prints an array of values
+     * in a python-like form.
+     */
     printf("[ ");
     for (int i = start; i < end; ++i) {
         printf("%d", values[i]);
@@ -72,6 +114,10 @@ void printList(int values[], int start, int end) {
 }
 
 void printlnList(int values[], int start, int end) {
+    /*
+     * Calls `printList` with same arguments and appends
+     * a newline character at the end.
+     */
     printList(values, start, end);
     printf("\n");
 }
